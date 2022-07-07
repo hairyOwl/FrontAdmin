e40f72ebc72ca1a2a60f9c02c2111a3b9e5bc6cf
hairyOwl <lxy_happy@126.com>
2022-07-07 11:28:58 +0800

    style(test.vue): 了解提案语法script-setup

---
+++ src/views/test.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description:
+ * @Author: hairyOwl
+ * @Date: 2022-07-04 17:35:15
+ * @LastEditors: hairyOwl
+ * @LastEditTime: 2022-07-04 17:41:32
+-->
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="inc">++</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const inc = () => {
+  count.value++;
+};
+</script>
